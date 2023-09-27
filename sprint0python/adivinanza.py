e1814383255383842602b598b450fad86a6b7e5e
Manuel Fernandez <mfernandezi22@fpcoruna.afundacion.org>
2023-09-27 11:44:39 +0200

    Commit Ultima Modificacion Adivinanzas

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,43 +1,51 @@
-adivinanzas = ["""
-Tengo una gran sombrilla
-Y me buscan por sabrosa
-Pero atención, ten cuidado
-Que puedo ser venenosa.
+import random
 
-a) Setas
-b) Parasol
-c) Sombra               
-""","""Doce caballeros
-nacidos del sol.
-Todos mueren antes
-de los treinta y dos.
+adivinanzas = [
+    """Tengo una gran sombrilla
+    Y me buscan por sabrosa
+    Pero atención, ten cuidado
+    Que puedo ser venenosa.
 
-a)Zodíaco
-b)Meses
-c)Apóstoles
-""","""Ciudadano muy mirado
-Moderno camaleón
-Subido en tu árbol
-Cambias de color.
+    a) Setas
+    b) Parasol
+    c) Sombra               
+    """,
+    """Doce caballeros
+    nacidos del sol.
+    Todos mueren antes
+    de los treinta y dos.
 
-a)Arcoíris
-b)Camaleón de Ciudad
-c)Semáforo"""]
+    a) Zodíaco
+    b) Meses
+    c) Apóstoles
+    """,
+    """Ciudadano muy mirado
+    Moderno camaleón
+    Subido en tu árbol
+    Cambias de color.
 
+    a) Arcoíris
+    b) Camaleón de Ciudad
+    c) Semáforo""",
+]
 
-respuestas_c = ['a','b','c']
+respuestas_c = ['a', 'b', 'c']
 puntuacion = 0
-num_adivinanza = 0
-for num_adivinanza in range(len(adivinanzas)):
-    print(adivinanzas[num_adivinanza])
+
+#índices aleatorios 
+indices_seleccionados = random.sample(range(len(adivinanzas)), 2)
+
+for indice in indices_seleccionados:
+    adivinanza = adivinanzas[indice]
+    print(adivinanza)
     respuesta = input()
-    if respuesta == respuestas_c[num_adivinanza]:
+
+    if respuesta == respuestas_c[indice]:
         puntuacion += 10
-        print('Respuesta Correcta')
-        print('')
+        print('Respuesta Correcta\n')
     else:
         if puntuacion > 0:
-            puntuacion-=5
-        print('Respuesta Incorrecta')
-        print('')
-print('Total de puntos ganados: '+str(puntuacion))    
+            puntuacion -= 5
+        print('Respuesta Incorrecta\n')
+
+print('Total de puntos ganados: ' + str(puntuacion))
