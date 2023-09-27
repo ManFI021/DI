ad08a44546fd301a517c71adb35da4cb74ea3397
Manuel Fernandez <mfernandezi22@fpcoruna.afundacion.org>
2023-09-27 10:56:38 +0200

    Segundo Commit Adivinanza

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -7,16 +7,37 @@ Que puedo ser venenosa.
 a) Setas
 b) Parasol
 c) Sombra               
-"""]
+""","""Doce caballeros
+nacidos del sol.
+Todos mueren antes
+de los treinta y dos.
+
+a)Zodíaco
+b)Meses
+c)Apóstoles
+""","""Ciudadano muy mirado
+Moderno camaleón
+Subido en tu árbol
+Cambias de color.
+
+a)Arcoíris
+b)Camaleón de Ciudad
+c)Semáforo"""]
 
 
-respuestas_c = ['a']
-print(adivinanzas[0])
-respuesta = input()
-if respuesta == respuestas_c[0]:
+respuestas_c = ['a','b','c']
+puntuacion = 0
+num_adivinanza = 0
+for num_adivinanza in range(len(adivinanzas)):
+    print(adivinanzas[num_adivinanza])
+    respuesta = input()
+    if respuesta == respuestas_c[num_adivinanza]:
+        puntuacion += 10
         print('Respuesta Correcta')
         print('')
-else:
+    else:
+        if puntuacion > 0:
+            puntuacion-=5
         print('Respuesta Incorrecta')
         print('')
-      
+print('Total de puntos ganados: '+str(puntuacion))    
