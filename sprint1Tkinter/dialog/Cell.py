efe56a5226c0858b0c257ef6f6c4ab3ec7b1d63e
Manuel Fernandez <mfernandezi22@fpcoruna.afundacion.org>
2023-10-10 09:47:56 +0200

    Tarea 6 Completada

--- sprint1Tkinter/dialog/Cell.py
+++ sprint1Tkinter/dialog/Cell.py
@@ -5,9 +5,10 @@ class Cell():
     def __init__(self, title, path):
         self.title = title
         self.path = path
-        self.image_tk = self.load_image()
-
-    def load_image(self):
-        image = Image.open(self.path)
-        image_tk = ImageTk.PhotoImage(image)
+        self.image_tk = self.load_and_resize_image()
+#funcion de resize de imagenes
+    def load_and_resize_image(self):
+        original_image = Image.open(self.path)
+        resized_image = original_image.resize((100,100),Image.LANCZOS)
+        image_tk = ImageTk.PhotoImage(resized_image)
         return image_tk
--- sprint1Tkinter/dialog/MainWindow.py
+++ sprint1Tkinter/dialog/MainWindow.py
@@ -1,7 +1,6 @@
-import tkinter as tk
 from tkinter import ttk, Tk, messagebox
-from PIL import Image, ImageTk
 from Cell import Cell
+import tkinter as tk
 
 class MainWindow:
     image_references = []
@@ -12,20 +11,18 @@ class MainWindow:
 
     def __init__(self, root):
         root.title("MainWindow")
-        #Ruta Imagenes edited
         self.cells = [
-            Cell("Victini", r"C:\msys64\home\Alumno\DI\sprint1Tkinter\catalog\data\edited\169px-Victini_(20_aniversario)_resized.png"),
-            Cell("Meme 1", r"C:\msys64\home\Alumno\DI\sprint1Tkinter\catalog\data\edited\IMG_20231004_140354_resized.jpg"),
-            Cell("Meme 2", r"C:\msys64\home\Alumno\DI\sprint1Tkinter\catalog\data\edited\IMG_20231004_140344_resized.jpg"),
-            Cell("Meme 3", r"C:\msys64\home\Alumno\DI\sprint1Tkinter\catalog\data\edited\a62e3935390bcf16d136c4760f087ad0_resized.jpg"),
-            Cell("Bmo", r"C:\msys64\home\Alumno\DI\sprint1Tkinter\catalog\data\edited\25f68acc5bfc7e5f149a714c6a660353_resized.png"),
+            Cell("Victini", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\victini.png"),
+            Cell("Meme 1", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\meme1.jpg"),
+            Cell("Meme 2", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\meme2.jpg"),
+            Cell("Meme 3", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\meme3.jpg"),
+            Cell("Bmo", "C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\bmo.png")
         ]
 
         for i, cell in enumerate(self.cells):
-            image_tk = ImageTk.PhotoImage(file=cell.path)
-            self.image_references.append(image_tk)
-            #Añadir la referencia de la imagen correspondiente para evitar PhotoImage Error
-            label = ttk.Label(root, image=image_tk, text=cell.title, compound=tk.BOTTOM)
+            self.image_references.append(cell.image_tk)
+
+            label = ttk.Label(root, image=cell.image_tk, text=cell.title, compound=tk.BOTTOM)
             label.grid(row=i, column=0)
             label.bind("<Button-1>", lambda event, celda=cell: self.on_button_clicked(celda))
 
