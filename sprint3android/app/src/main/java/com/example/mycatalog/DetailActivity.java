568732af303c2249bdca71d3784bfacb3fc7b04a
Manuel Fernandez <mfernandezi22@fpcoruna.afundacion.org>
2023-11-07 10:17:53 +0100

    Tarea 4 Completada y Corregido xml Constraints y Inflation

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -3,12 +3,31 @@ package com.example.mycatalog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.detail_activity);
+
+        // Obtener referencias a los elementos de la interfaz de usuario
+        TextView tituloTextView = findViewById(R.id.tituloTextView);
+        ImageView imagenView = findViewById(R.id.imagenView);
+        TextView descripcionTextView = findViewById(R.id.descripcionTextView);
+        Button meGustaButton = findViewById(R.id.meGustaButton);
+        meGustaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Aquí puedes agregar el código para manejar el evento del botón "Me gusta".
+            }
+        });
+        tituloTextView.setText("Pingu");
+        descripcionTextView.setText("Sinceramente demuestra mi frustación con tantos trabajos de manera no proporcional a horas de sueño :)");
+        imagenView.setImageResource(R.drawable.pingu);
     }
 }
